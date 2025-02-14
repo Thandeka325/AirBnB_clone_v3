0accb5b1454a0816d3ec89681105f1ef68759946
Thandeka325 <marhwayiza325@gmail.com>
2025-02-14 11:52:57 -0800

    Project: 0x05. AirBnB clone - RESTful API

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,74 +1,110 @@
 #!/usr/bin/python3
 """
-API endpoints for City objects.
+API actions for Cites
 """
-
 from flask import jsonify, abort, request
-from api.v1.views import app_views
-from models import storage
-from models.state import State
+from api.v1.views import app_views, storage
 from models.city import City
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'],
+@app_views.route("/states/<state_id>/cities", methods=["GET"],
                  strict_slashes=False)
-def get_cities(state_id):
-    """Retrieves the list of all City objects of a State."""
-    state = storage.get(State, state_id)
-    if not state:
+def city_by_state(state_id):
+    """
+    retrieves all City objects from a specific state
+    :return: json of all cities in a state or 404 on error
+    """
+    city_list = []
+    state_obj = storage.get("State", state_id)
+
+    if state_obj is None:
         abort(404)
-    return jsonify([city.to_dict() for city in state.cities])
+    for obj in state_obj.cities:
+        city_list.append(obj.to_json())
+
+    return jsonify(city_list)
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
-def get_city(city_id):
-    """Retrieves a City object."""
-    city = storage.get(City, city_id)
-    if not city:
-        abort(404)
-    return jsonify(city.to_dict())
-
-
-@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
-def delete_city(city_id):
-    """Deletes a City object."""
-    city = storage.get(City, city_id)
-    if not city:
-        abort(404)
-    storage.delete(city)
-    storage.save()
-    return jsonify({}), 200
-
-
-@app_views.route('/states/<state_id>/cities', methods=['POST'],
+@app_views.route("/states/<state_id>/cities", methods=["POST"],
                  strict_slashes=False)
-def create_city(state_id):
-    """Creates a City."""
-    state = storage.get(State, state_id)
-    if not state:
+def city_create(state_id):
+    """
+    create city route
+    param: state_id - state id
+    :return: newly created city obj
+    """
+    city_json = request.get_json(silent=True)
+    if city_json is None:
+        abort(400, 'Not a JSON')
+
+    if not storage.get("State", str(state_id)):
         abort(404)
-    city_data = request.get_json()
-    if not city_data:
-        abort(400, description="Not a JSON")
-    if 'name' not in city_data:
-        abort(400, description="Missing name")
-    city = City(state_id=state.id, **city_data)
-    storage.new(city)
-    storage.save()
-    return jsonify(city.to_dict()), 201
+
+    if "name" not in city_json:
+        abort(400, 'Missing name')
+
+    city_json["state_id"] = state_id
+
+    new_city = City(**city_json)
+    new_city.save()
+    resp = jsonify(new_city.to_json())
+    resp.status_code = 201
+
+    return resp
 
 
-@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
-def update_city(city_id):
-    """Updates a City object."""
-    city = storage.get(City, city_id)
-    if not city:
+@app_views.route("/cities/<city_id>",  methods=["GET"],
+                 strict_slashes=False)
+def city_by_id(city_id):
+    """
+    gets a specific City object by ID
+    :param city_id: city object id
+    :return: city obj with the specified id or error
+    """
+
+    fetched_obj = storage.get("City", str(city_id))
+
+    if fetched_obj is None:
         abort(404)
-    city_data = request.get_json()
-    if not city_data:
-        abort(400, description="Not a JSON")
-    for key, value in city_data.items():
-        if key not in ['id', 'state_id', 'created_at', 'updated_at']:
-            setattr(city, key, value)
+
+    return jsonify(fetched_obj.to_json())
+
+
+@app_views.route("cities/<city_id>",  methods=["PUT"], strict_slashes=False)
+def city_put(city_id):
+    """
+    updates specific City object by ID
+    :param city_id: city object ID
+    :return: city object and 200 on success, or 400 or 404 on failure
+    """
+    city_json = request.get_json(silent=True)
+    if city_json is None:
+        abort(400, 'Not a JSON')
+    fetched_obj = storage.get("City", str(city_id))
+    if fetched_obj is None:
+        abort(404)
+    for key, val in city_json.items():
+        if key not in ["id", "created_at", "updated_at", "state_id"]:
+            setattr(fetched_obj, key, val)
+    fetched_obj.save()
+    return jsonify(fetched_obj.to_json())
+
+
+@app_views.route("/cities/<city_id>",  methods=["DELETE"],
+                 strict_slashes=False)
+def city_delete_by_id(city_id):
+    """
+    deletes City by id
+    :param city_id: city object id
+    :return: empty dict with 200 or 404 if not found
+    """
+
+    fetched_obj = storage.get("City", str(city_id))
+
+    if fetched_obj is None:
+        abort(404)
+
+    storage.delete(fetched_obj)
     storage.save()
-    return jsonify(city.to_dict()), 200
+
+    return jsonify({})
--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,77 +1,105 @@
 #!/usr/bin/python3
-"""Handles all default RESTful API actions for Place-Amenity relationship"""
+"""
+API actions for Place Amenities
+"""
 from flask import jsonify, abort
-from api.v1.views import app_views
-from models import storage
-from models.place import Place
-from models.amenity import Amenity
 from os import getenv
 
-storage_type = getenv("HBNB_TYPE_STORAGE")
+from api.v1.views import app_views, storage
 
 
-@app_views.route('/places/<place_id>/amenities', methods=['GET'],
+@app_views.route("/places/<place_id>/amenities",
+                 methods=["GET"],
                  strict_slashes=False)
-def get_place_amenities(place_id):
-    """Retrieves all Amenity objects of a Place"""
-    place = storage.get(Place, place_id)
-    if not place:
+def amenity_by_place(place_id):
+    """
+    get all amenities of a place
+    :param place_id: amenity id
+    :return: all amenities
+    """
+    fetched_obj = storage.get("Place", str(place_id))
+
+    all_amenities = []
+
+    if fetched_obj is None:
         abort(404)
 
-    if storage_type == "db":
-        amenities = place.amenities
+    for obj in fetched_obj.amenities:
+        all_amenities.append(obj.to_json())
+
+    return jsonify(all_amenities)
+
+
+@app_views.route("/places/<place_id>/amenities/<amenity_id>",
+                 methods=["DELETE"],
+                 strict_slashes=False)
+def unlink_amenity_from_place(place_id, amenity_id):
+    """
+    unlinks an amenity in a place
+    :param place_id: place id
+    :param amenity_id: amenity id
+    :return: empty dict or error
+    """
+    if not storage.get("Place", str(place_id)):
+        abort(404)
+    if not storage.get("Amenity", str(amenity_id)):
+        abort(404)
+
+    fetched_obj = storage.get("Place", place_id)
+    found = 0
+
+    for obj in fetched_obj.amenities:
+        if str(obj.id) == amenity_id:
+            if getenv("HBNB_TYPE_STORAGE") == "db":
+                fetched_obj.amenities.remove(obj)
+            else:
+                fetched_obj.amenity_ids.remove(obj.id)
+            fetched_obj.save()
+            found = 1
+            break
+
+    if found == 0:
+        abort(404)
     else:
-        amenities = [storage.get(Amenity, amenity_id)
-                     for amenity_id in place.amenity_ids]
+        resp = jsonify({})
+        resp.status_code = 201
+        return resp
 
-    return jsonify([amenity.to_dict() for amenity in amenities])
 
+@app_views.route("/places/<place_id>/amenities/<amenity_id>",
+                 methods=["POST"],
+                 strict_slashes=False)
+def link_amenity_to_place(place_id, amenity_id):
+    """
+    links a amenity with a place
+    :param place_id: place id
+    :param amenity_id: amenity id
+    :return: return Amenity obj added or error
+    """
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE'], strict_slashes=False)
-def delete_place_amenity(place_id, amenity_id):
-    """Deletes an Amenity object from a Place"""
-    place = storage.get(Place, place_id)
-    if not place:
+    fetched_obj = storage.get("Place", str(place_id))
+    amenity_obj = storage.get("Amenity", str(amenity_id))
+    found_amenity = None
+
+    if not fetched_obj or not amenity_obj:
         abort(404)
 
-    amenity = storage.get(Amenity, amenity_id)
-    if not amenity:
-        abort(404)
+    for obj in fetched_obj.amenities:
+        if str(obj.id) == amenity_id:
+            found_amenity = obj
+            break
 
-    if storage_type == "db":
-        if amenity not in place.amenities:
-            abort(404)
-        place.amenities.remove(amenity)
+    if found_amenity is not None:
+        return jsonify(found_amenity.to_json())
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        fetched_obj.amenities.append(amenity_obj)
     else:
-        if amenity_id not in place.amenity_ids:
-            abort(404)
-        place.amenity_ids.remove(amenity_id)
+        fetched_obj.amenities = amenity_obj
 
-    storage.save()
-    return jsonify({}), 200
+    fetched_obj.save()
 
+    resp = jsonify(amenity_obj.to_json())
+    resp.status_code = 201
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['POST'], strict_slashes=False)
-def link_place_amenity(place_id, amenity_id):
-    """Links an Amenity object to a Place"""
-    place = storage.get(Place, place_id)
-    if not place:
-        abort(404)
-
-    amenity = storage.get(Amenity, amenity_id)
-    if not amenity:
-        abort(404)
-
-    if storage_type == "db":
-        if amenity in place.amenities:
-            return jsonify(amenity.to_dict()), 200
-        place.amenities.append(amenity)
-    else:
-        if amenity_id in place.amenity_ids:
-            return jsonify(amenity.to_dict()), 200
-        place.amenity_ids.append(amenity_id)
-
-    storage.save()
-    return jsonify(amenity.to_dict()), 201
+    return resp
