94230b23053139b1fb16aa7b524f7b5cc0228155
Thandeka325 <marhwayiza325@gmail.com>
2025-02-14 10:47:43 -0800

    Project: 0x05. AirBnB clone - RESTful API

--- models/user.py
+++ models/user.py
@@ -6,10 +6,11 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
+    """Representation of a user"""
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
@@ -25,5 +26,21 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """Initializes user, hashes password if provided"""
+        if "password" in kwargs:
+            kwargs["password"] = self.hash_password(kwargs["password"])
         super().__init__(*args, **kwargs)
+
+    def hash_password(self, password):
+        """Hashes a password using MD5"""
+        return hashlib.md5(password.encode()).hexdigest()
+
+    @property
+    def password(self):
+        """Password getter (disabled for security reasons)"""
+        return None
+
+    @password.setter
+    def password(self, pwd):
+        """Hashes and sets the password"""
+        self.__dict__["password"] = self.hash_password(pwd)
