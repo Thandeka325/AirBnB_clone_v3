b9277531d371db278669583f79c0b84e47e931f8
Thandeka325 <marhwayiza325@gmail.com>
2025-02-14 04:05:00 -0800

    Project: 0x05. AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,10 +13,12 @@ app = Flask(__name__)
 # Register blueprint
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes storage session"""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,8 +6,8 @@ Index route for API v1.
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns the API status"""
     return jsonify({"status": "OK"})
-
