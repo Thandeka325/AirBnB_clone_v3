df8993029d13f81eccc3a1419792b55d8a37a3ff
Thandeka325 <marhwayiza325@gmail.com>
2025-02-14 04:33:07 -0800

    Project: 0x05. AirBnB clone - RESTful API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
 """
-Index route for API v1.
+Module to define the status and stats routes
 """
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
-def status():
-    """Returns the API status"""
+def get_status():
+    """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Retrieves the number of each object by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(stats)
