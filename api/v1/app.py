209834b8b4855f4a1954e610a07f2baf6a5fd5b4
Thandeka325 <marhwayiza325@gmail.com>
2025-02-14 10:24:09 -0800

    Project: 0x05. AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-"""
-Flask app setup for the API
-"""
-
+"""Flask app for AirBnB API"""
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-import os
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+
+# Register Blueprint
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+# Enable CORS for all routes and allow requests from all origins (0.0.0.0)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Closes the storage session"""
+    """Close the storage session"""
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Handles 404 errors and returns a JSON response"""
+    """Return JSON response for 404 errors"""
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", "5000"))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
