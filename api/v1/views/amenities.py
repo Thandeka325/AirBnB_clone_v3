90ac93ff5e63905577c9e2022527cfdc19a9a3c1
Thandeka325 <marhwayiza325@gmail.com>
2025-02-18 05:34:01 -0800

    Project: 0x05. AirBnB clone - RESTful API

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -3,81 +3,97 @@
 API actions for Amenity objects
 """
 from flask import jsonify, abort, request
-from api.v1.views import app_views
-from models import storage
+from api.v1.views import app_views, storage
 from models.amenity import Amenity
 
 
 @app_views.route("/amenities", methods=["GET"], strict_slashes=False)
 def amenity_get_all():
     """
-    Retrieves the list of all Amenity objects
+    retrieves all Amenity objects
+    :return: json of all states
     """
-    amenities = storage.all(Amenity).values()
-    return jsonify([amenity.to_dict() for amenity in amenities])
+    am_list = []
+    am_obj = storage.all("Amenity")
+    for obj in am_obj.values():
+        am_list.append(obj.to_json())
 
-
-@app_views.route("/amenities/<amenity_id>", methods=["GET"],
-                 strict_slashes=False)
-def amenity_by_id(amenity_id):
-    """
-    Retrieves a specific Amenity object by ID
-    """
-    fetched_obj = storage.get(Amenity, amenity_id)
-    if fetched_obj is None:
-        abort(404)
-
-    return jsonify(fetched_obj.to_dict())
+    return jsonify(am_list)
 
 
 @app_views.route("/amenities", methods=["POST"], strict_slashes=False)
 def amenity_create():
     """
-    Creates a new Amenity object
+    create amenity route
+    :return: newly created amenity obj
     """
-    am_json = request.get_json()
-    if not am_json:
+    am_json = request.get_json(silent=True)
+    if am_json is None:
         abort(400, 'Not a JSON')
     if "name" not in am_json:
         abort(400, 'Missing name')
 
     new_am = Amenity(**am_json)
     new_am.save()
-    return jsonify(new_am.to_dict()), 201
+    resp = jsonify(new_am.to_json())
+    resp.status_code = 201
+
+    return resp
 
 
-@app_views.route("/amenities/<amenity_id>", methods=["PUT"],
+@app_views.route("/amenities/<amenity_id>",  methods=["GET"],
                  strict_slashes=False)
-def amenity_put(amenity_id):
+def amenity_by_id(amenity_id):
     """
-    Updates an existing Amenity object
+    gets a specific Amenity object by ID
+    :param amenity_id: amenity object id
+    :return: state obj with the specified id or error
     """
-    fetched_obj = storage.get(Amenity, amenity_id)
+
+    fetched_obj = storage.get("Amenity", str(amenity_id))
+
     if fetched_obj is None:
         abort(404)
 
-    am_json = request.get_json()
-    if not am_json:
-        abort(400, 'Not a JSON')
+    return jsonify(fetched_obj.to_json())
 
+
+@app_views.route("/amenities/<amenity_id>",  methods=["PUT"],
+                 strict_slashes=False)
+def amenity_put(amenity_id):
+    """
+    updates specific Amenity object by ID
+    :param amenity_id: amenity object ID
+    :return: amenity object and 200 on success, or 400 or 404 on failure
+    """
+    am_json = request.get_json(silent=True)
+    if am_json is None:
+        abort(400, 'Not a JSON')
+    fetched_obj = storage.get("Amenity", str(amenity_id))
+    if fetched_obj is None:
+        abort(404)
     for key, val in am_json.items():
         if key not in ["id", "created_at", "updated_at"]:
             setattr(fetched_obj, key, val)
-
     fetched_obj.save()
-    return jsonify(fetched_obj.to_dict()), 200
+    return jsonify(fetched_obj.to_json())
 
 
-@app_views.route("/amenities/<amenity_id>", methods=["DELETE"],
+@app_views.route("/amenities/<amenity_id>",  methods=["DELETE"],
                  strict_slashes=False)
 def amenity_delete_by_id(amenity_id):
     """
-    Deletes an Amenity by ID
+    deletes Amenity by id
+    :param amenity_id: Amenity object id
+    :return: empty dict with 200 or 404 if not found
     """
-    fetched_obj = storage.get(Amenity, amenity_id)
+
+    fetched_obj = storage.get("Amenity", str(amenity_id))
+
     if fetched_obj is None:
         abort(404)
 
     storage.delete(fetched_obj)
     storage.save()
-    return jsonify({}), 200
+
+    return jsonify({})
