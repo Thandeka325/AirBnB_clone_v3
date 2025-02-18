2fd686c5df58f821973c20bf8b2d909fb15aef58
Thandeka325 <marhwayiza325@gmail.com>
2025-02-18 12:58:10 -0800

    Project: 0x05. AirBnB clone - RESTful API

--- models/user.py
+++ models/user.py
@@ -1,52 +1,38 @@
 #!/usr/bin/python3
 """
-Initialize the User class model
+Initialize the User class
 """
-import os
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Float
-from hashlib import md5
-storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+import hashlib
 
 
 class User(BaseModel, Base):
-    """User class handles all application users"""
-    if storage_type == "db":
+    """Representation of a user """
+    if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column("password", String(128), nullable=False)
+        password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-
-        places = relationship('Place', backref='user', cascade='delete')
-        reviews = relationship('Review', backref='user', cascade='delete')
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """
-        initialize User Model, inherits from BaseModel
-        """
-        if 'password' in kwargs:
-            kwargs['password'] = (
-                    md5(kwargs['password'].encode('utf-8')).hexidigest()
-            )
+        """initializes user"""
         super().__init__(*args, **kwargs)
 
-    @property
-    def password(self):
-        """
-        getter for password
-        """
-        return self.__dict__.get("password")
-
-    @password.setter
-    def password(self, password):
-        """
-        Setter for password that hashes the password using MD5
-        """
-        self.__dict__["password"] = md5(value.encode('utf-8')).hexdigest()
+    def __setattr__(self, name, value):
+        """Override setattr to hash password when it's set"""
+        if name == "password":
+            value = hashlib.md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
